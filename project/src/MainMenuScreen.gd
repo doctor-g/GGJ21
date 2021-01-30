a81487fa732bef9fe4e45410303aeb51933f6bea
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-30 18:13:56 -0500

    Make the buttons all regularly sized
    Right now, it's not much better, but once we add the other animals,
    it will be.

--- project/src/MainMenuScreen.gd
+++ project/src/MainMenuScreen.gd
@@ -3,12 +3,23 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(0,GameState.unlock_level+1):
+	for i in range(0,AnimalSettings.ANIMALS.size()):
 		var button := Button.new()
-		button.icon = AnimalSettings.ANIMALS[i].image
 		button.connect("pressed", self, "_on_Button_pressed", [i])
+		button.size_flags_horizontal = SIZE_EXPAND_FILL
+		button.size_flags_vertical = SIZE_EXPAND_FILL
+		if i <= GameState.unlock_level:
+			var sprite := Sprite.new()
+			sprite.centered = false
+			sprite.texture = AnimalSettings.ANIMALS[i].image
+			var linear_scale := 100.0 / sprite.texture.get_width()
+			sprite.scale = Vector2(linear_scale, linear_scale)
+			button.add_child(sprite)
+		else:
+			button.disabled = true
+			button.text = "?"
 		$AnimalGrid.add_child(button)
-
+		
 
 func _on_Button_pressed(animal_index)->void:
 	GameState.reset()
