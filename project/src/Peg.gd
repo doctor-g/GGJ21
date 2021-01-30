31cbbcab0dd61609e3989d4cfc6ab385ebcb048d
Alex <alex.gestwicki@gmail.com>
2021-01-30 10:13:30 -0500

    Make one-third of pegs have three health

--- project/src/Peg.gd
+++ project/src/Peg.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 signal destroyed()
 
-export var _health := 3
+export var health := 1
 
 onready var _colored_part := $PegBackground
 onready var _tween := $Tween
@@ -11,15 +11,15 @@ onready var _tween := $Tween
 var _health_colors := [Color.blue, Color.orange, Color.red]
 
 func _ready():
-	_colored_part.modulate = _health_colors[_health-1]
+	_colored_part.modulate = _health_colors[health-1]
 
 func hit():
-	_health -= 1
-	if _health > 0:
-		var next_color:Color = _health_colors[_health-1]
+	health -= 1
+	if health > 0:
+		var next_color:Color = _health_colors[health-1]
 		_tween.interpolate_property(_colored_part, "modulate", null, next_color, 0.5)
 		_tween.start()
-	if _health <= 0:
+	if health <= 0:
 		emit_signal("destroyed")
 		queue_free()
 
--- project/src/PegGenerator.gd
+++ project/src/PegGenerator.gd
@@ -5,16 +5,20 @@ signal pegs_ready(pegs)
 
 const _peg := preload("res://src/Peg.tscn")
 
-export var _max_pegs := 20
+export var _max_pegs := 3.0
 export var _min_distance_between_pegs := 40.0
+export var _percent_of_upgraded_pegs := 33.0
 
 var _peg_positions := []
+var _upgraded_pegs:int = 0
 
 var _spawn_area_offset := Vector2.ZERO
 var _spawn_area_bounds := Vector2.ZERO
 
 func _ready():
 	randomize()
+	_percent_of_upgraded_pegs /= 100
+	_upgraded_pegs = ceil(_max_pegs*_percent_of_upgraded_pegs)
 	var _spawn_area : Vector2 = $SpawnArea/CollisionShape2D.shape.extents*2
 	_spawn_area_offset = (get_viewport_rect().size-_spawn_area)/2
 	_spawn_area_bounds = _spawn_area
@@ -36,11 +40,14 @@ func _generate_pegs():
 					print("Too Close")
 		_peg_positions.append(pos)
 		var _Peg := _peg.instance()
+		if _upgraded_pegs > 0:
+			_upgraded_pegs -= 1
+			_Peg.health = 3
 		_Peg.position = pos
 		$Pegs.add_child(_Peg)
 	for peg in $Pegs.get_children():
 		var _ignore = peg.connect("destroyed", self, "_on_peg_destroyed")
+	emit_signal("pegs_ready", _max_pegs)
 
 func _on_peg_destroyed():
-	emit_signal("pegs_ready", _max_pegs)
 	emit_signal("peg_destroyed")
