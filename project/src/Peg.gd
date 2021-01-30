e3fb6fee9a49b42c00dbe28379b8f1ab1386b1d4
Alex <alex.gestwicki@gmail.com>
2021-01-30 10:18:32 -0500

    Change peg color by going to white and then back

--- project/src/Peg.gd
+++ project/src/Peg.gd
@@ -16,10 +16,16 @@ func _ready():
 func hit():
 	health -= 1
 	if health > 0:
+		_tween.interpolate_property(_colored_part, "modulate", null, Color.white, 0.05)
+		_tween.start()
+		yield(get_tree().create_timer(0.05), "timeout")
 		var next_color:Color = _health_colors[health-1]
-		_tween.interpolate_property(_colored_part, "modulate", null, next_color, 0.5)
+		_tween.interpolate_property(_colored_part, "modulate", null, next_color, 0.05)
 		_tween.start()
 	if health <= 0:
+		_tween.interpolate_property(_colored_part, "modulate", null, Color.white, 0.05)
+		_tween.start()
+		yield(get_tree().create_timer(0.05), "timeout")
 		emit_signal("destroyed")
 		queue_free()
 
