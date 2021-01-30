e0c8a4ccfa285ee6c7d971b39d7e2b113bbcce48
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-30 13:43:03 -0500

    Use the new paddle image

--- project/src/Target.gd
+++ project/src/Target.gd
@@ -7,11 +7,3 @@ func _ready():
 	# This allows us to change the physics properties of a kinematic body. Cool.
 	Physics2DServer.body_set_param(get_rid(), Physics2DServer.BODY_PARAM_BOUNCE, bounciness)
 	
-
-func _draw():
-	var radius = $CollisionShape2D.shape.radius
-	var height = $CollisionShape2D.shape.height
-	var color = Color.aliceblue
-	draw_circle(Vector2(height/2,0), radius, color)
-	draw_circle(-Vector2(height/2,0), radius, color)
-	draw_rect(Rect2(-Vector2(height, radius*2)/2, Vector2(height, radius*2)), color)
