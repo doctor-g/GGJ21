f9d575f54bd3add463b631170ed314ba7d42e803
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-30 11:40:21 -0500

    Update the bounciness of the 'target'

--- project/src/Target.gd
+++ project/src/Target.gd
@@ -1,5 +1,12 @@
 extends KinematicBody2D
 
+export (float, 0, 1, 0.1) var bounciness := 1.0
+
+func _ready():
+	# See https://godotengine.org/qa/79826/adjusting-the-physical-material-of-kinematicbody
+	# This allows us to change the physics properties of a kinematic body. Cool.
+	Physics2DServer.body_set_param(get_rid(), Physics2DServer.BODY_PARAM_BOUNCE, bounciness)
+	
 
 func _draw():
 	var radius = $CollisionShape2D.shape.radius
