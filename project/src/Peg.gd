340d73601b6b62ba334066916ed51f4386d4660a
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-30 07:12:55 -0500

    Adjust vertical spacing to follow GDQuest guidelines

--- project/src/Peg.gd
+++ project/src/Peg.gd
@@ -10,12 +10,14 @@ var _level := 0
 func _ready():
 	_level = _health
 
+
 func hit():
 	_health -= 1
 	if _health <= 0:
 		emit_signal("destroyed", _level)
 		print("SQUISHED A PEG")
 		queue_free()
+
 
 func _draw():
 	draw_circle(Vector2.ZERO, 10, Color.green)
