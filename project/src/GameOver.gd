b1f5bd2662b3c1fb4893eb36f5f21f36d4d3f7ec
Alex <alex.gestwicki@gmail.com>
2021-01-30 15:03:10 -0500

    Improve end game a little bit more

--- project/src/GameOver.gd
+++ project/src/GameOver.gd
@@ -4,12 +4,19 @@ signal dismissed
 
 var _score := 0
 
-func _process(_delta):
+func _ready():
+	$HighScore.text = ""
+	$YourScore.text = ""
+	$NewHigh.text = ""
 	GameState.connect("score_changed", self, "score_changed")
-	if GameState.highscore == _score:
-		$Label2.text = "New High Score! "+str(GameState.highscore)
-	else:
-		$Label2.text = "High Score: "+str(GameState.highscore)+", Your Score: "+str(_score)
+
+func _process(_delta):
+	if visible:
+		if GameState.highscore == _score:
+			$NewHigh.text = "New High Score! "+str(GameState.highscore)
+		else:
+			$HighScore.text = "High Score: "+str(GameState.highscore)
+			$YourScore.text = "Your Score: "+str(_score)
 
 
 func score_changed(score:int):
