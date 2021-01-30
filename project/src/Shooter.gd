550aaf830d64b2fd706e970ce1e878c1331d624d
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-30 07:39:50 -0500

    Update shooter to show straight-line launch path

--- project/src/Shooter.gd
+++ project/src/Shooter.gd
@@ -2,31 +2,46 @@ extends Node2D
 
 enum _State {IDLE, LOADED, AIMING}
 
-const _ball := preload("res://src/Ball.tscn")
+const _Ball := preload("res://src/Ball.tscn")
 
 export var _initial_impulse := 400
 export var _one_shot := false
 
-var _state = _State.AIMING
+var _state = _State.IDLE
+var _ball : RigidBody2D
+var _mouse_pos : Vector2
+
 
 func _input(event:InputEvent):
 	if event is InputEventMouseButton:
-		if event.pressed and _state == _State.AIMING:
-			_state = _State.LOADED
-		elif not event.pressed and _state == _State.LOADED:
-			if _one_shot:
-				_state = _State.IDLE
-			else:
-				_state = _State.AIMING
+		if event.pressed and _state == _State.IDLE:
+			_load_ball()
+		elif not event.pressed and _state == _State.AIMING:
 			_shoot_ball()
 
-func _process(_delta):
-	if _state != _State.IDLE:
-		var mouse_pos = get_global_mouse_position()
-		rotation = mouse_pos.angle()
 
+func _load_ball():
+	_state = _State.AIMING
+	_ball = _Ball.instance()
+	_ball.mode = RigidBody2D.MODE_STATIC
+	_ball.position = get_global_transform().origin
+	get_parent().add_child(_ball)
+
+
+func _process(_delta):
+	if _state == _State.AIMING:
+		_mouse_pos = get_viewport().get_mouse_position()
+		update()
+
+func _draw():
+	if _state == _State.AIMING:
+		draw_line(Vector2.ZERO, _mouse_pos - get_global_transform().get_origin(), Color.black, 5)
+		
+	
 func _shoot_ball():
-	var _Ball:RigidBody2D = _ball.instance()
-	_Ball.position = get_global_transform().origin
-	get_tree().root.add_child(_Ball)
-	_Ball.apply_impulse(Vector2.ZERO, Vector2(_initial_impulse, 0).rotated(rotation))
+	update() # Force redraw to eliminate drawn line
+	_ball.mode = RigidBody2D.MODE_RIGID
+	var angle = (_mouse_pos - get_global_transform().get_origin()).angle()
+	var impulse_vector := Vector2(_initial_impulse, 0).rotated(angle)
+	_ball.apply_impulse(Vector2.ZERO, impulse_vector)
+	_state = _State.IDLE
