95539916d5ad6550ae2b8e97a14bf9c4e9589905
Alex <alex.gestwicki@gmail.com>
2021-01-30 13:56:08 -0500

    Mess with ball trail

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,6 +1,11 @@
 class_name Ball 
 extends RigidBody2D
 
+var alpha
+
+func _process(delta):
+	alpha = lerp(0,1,clamp(linear_velocity.length_squared()/300000, 0, 1))
+	$CPUParticles2D.modulate = Color(1,1,1,alpha)
 
 func _on_Ball_body_shape_entered(_body_id, body, _body_shape, _local_shape):
 	if body is Peg:
