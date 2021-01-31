7831ef52c1ce47db29d0f79d555d21c26855229c
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-31 07:47:31 -0500

    Add button click sound

--- project/src/ConfirmationDialog.gd
+++ project/src/ConfirmationDialog.gd
@@ -3,12 +3,16 @@ extends Control
 signal confirmed
 signal cancelled
 
+onready var _button_click := $ButtonClick
+
 
 func _on_YesButton_pressed():
+	_button_click.play()
 	emit_signal("confirmed")
 	queue_free()
 
 
 func _on_NoButton_pressed():
+	_button_click.play()
 	emit_signal("cancelled")
 	queue_free()
