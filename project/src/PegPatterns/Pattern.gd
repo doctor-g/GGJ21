6f5e136e89ab852a242482b4e66bff80fc40479e
Alex <alex.gestwicki@gmail.com>
2021-01-31 10:51:25 -0500

    Make animations start out of sync

--- project/src/PegPatterns/Pattern.gd
+++ project/src/PegPatterns/Pattern.gd
@@ -6,6 +6,8 @@ func _ready():
 	for child in get_children():
 		if child is AnimationPlayer:
 			child.play("Anim")
+			var seek_to:int = randi()%int(child.current_animation_length)
+			child.seek(seek_to, true)
 
 func _process(delta):
 	if _rotating:
