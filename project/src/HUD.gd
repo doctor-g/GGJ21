1374ca6687d3d65d9b5c8a2c97595c414ca8e278
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-01-31 12:38:44 -0500

    Animate score transitions

--- project/src/HUD.gd
+++ project/src/HUD.gd
@@ -4,6 +4,10 @@ onready var _score_label := $ScoreBox/ScoreLabel
 onready var _lives_label := $LivesBox/LivesLabel
 onready var _chain_label := $ChainLabel
 
+export var lerp_percent_per_frame := 0.3
+
+var _target_score := 0
+var _displayed_score := 0
 
 func _ready():
 	GameState.connect("score_changed", self, "_on_score_changed")
@@ -14,8 +18,13 @@ func _ready():
 	_chain_label.text = str(GameState.get_chain())
 
 
+func _physics_process(_delta):
+	_displayed_score = lerp(_displayed_score, _target_score, lerp_percent_per_frame)
+	_score_label.text = str(_displayed_score)
+
+
 func _on_score_changed(score:int)->void:
-	_score_label.text = str(score)
+	_target_score = score
 
 
 func _on_lives_changed(lives:int)->void:
